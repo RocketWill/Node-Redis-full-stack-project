05e8a2df590d4533e881ab5fae618bc7a075d097
Will Cheng Yong <chengyong@pku.edu.cn>
2019-11-22 11:06:45 +0800

    Complete server side get random Pokemons data

--- server-side/src/api/controllers/getRandomPokemons.ts
+++ server-side/src/api/controllers/getRandomPokemons.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response, response } from "express";
 
 const axios = require("axios");
+const fetch = require("node-fetch");
 
 const getRandomNumbersList = (
   min: number,
@@ -11,37 +12,49 @@ const getRandomNumbersList = (
   while (resArr.length < total) {
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     if (resArr.indexOf(randomNumber) === -1) {
-      resArr = [...resArr, axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)]
+      resArr.push(randomNumber);
     }
   }
   return resArr;
 };
 
-const getPokemons = (lst: any[]): any => {
-  const result: any[] = [];
+const sortOutData = (rawData) => {
+  const id = rawData['id'];
+  const profile = {
+    weight: rawData['weight'],
+    height: rawData['height'],
+    abilities: rawData['abilities'].map(ability => ability['ability']),
+  }
+  const held_items = rawData['held_items'].map(item => item['item'])
+  const stats = rawData['stats'].map(stat => ({name: stat['stat']['name'], value: stat['base_stat']}))
+  const sprite = rawData['sprites']['front_default']
+  const name = rawData['name']
+  const types = rawData['types'].map(type => type['type'])
 
-  axios
-    .all(lst)
-    .then(
-      axios.spread((...responses) => {
-        let resp = [];
-        console.log(responses.length);
-        responses.map(d => {
-          resp = [...resp, d.data]
-        })
-        return resp;
-      })
-    )
-    .catch(errors => {
-      // react on errors.
-      console.error(errors);
-    });
+  return({
+    id,
+    profile,
+    held_items,
+    stats,
+    sprite,
+    name,
+    types
+  })
+}
 
-};
+const getPokemons = lst => {
+  const pokemonsInfo = lst.map( pokemonId => (
+    axios (`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(d => d.data)
+  ))
+  return pokemonsInfo;
+}
 
 export function getRandomPokemons(req: Request, res: Response) {
-  const lst = getPokemons(getRandomNumbersList(0, 300, 10));
-  const pokemonList = getPokemons(lst);
-  console.log(pokemonList)
-  res.send(pokemonList);
+  const lst = getRandomNumbersList(0, 300, 10);
+  const pokemonList = getPokemons(lst)
+  Promise.all(pokemonList).then((values) => {
+    const finalData = values.map(value => sortOutData(value))
+    res.send(finalData)
+  });
 }
