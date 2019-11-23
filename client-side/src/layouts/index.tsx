0ec65b7fd6d1e8b62be3ca54a09dd546a5be77c4
Yong Cheng <will.chengyong@gmail.com>
2019-11-24 01:46:45 +0800

    Update

--- client-side/src/layouts/index.tsx
+++ client-side/src/layouts/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './index.css';
 import pokemonLogo from '../assets/pokemon-logo.png';
+import githubLogo from '../assets/GitHub_Logo_White.png';
+
 
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
-      
-      <h1 className={styles.title}><img src={pokemonLogo} alt="pokemon logo" style={{ width: 200 }} /> <span style={{ fontSize: 20, letterSpacing: 1 }}>with Redis</span></h1>
+      <h1 className={styles.title}>
+        <img src={pokemonLogo} alt="pokemon logo" style={{ width: 200, display: "inline-block" }} />{' '}
+        <span style={{ fontSize: 20, letterSpacing: 1, display: "inline-block" }}>with Redis</span>
+        <a href="https://github.com/RocketWill/Node-Redis-full-stack-project" target="_blank"><img style={{width: 60, display: "inline-block", float: "right", marginRight: 20 }} src={githubLogo} /></a>
+      </h1>
       <div className={styles.content}>{props.children}</div>
+      
     </div>
   );
 };
